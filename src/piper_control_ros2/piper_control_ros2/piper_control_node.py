64e55c2b60fdaf54738821e69e15392bd35d8d5f
Akhil Raju <akhil@reimaginerobotics.ai>
2026-01-20 09:39:01 +0000

    Reduce the j3 gain slightly for smoother control at lower frequencies. (#34)

--- src/piper_control_ros2/piper_control_ros2/piper_control_node.py
+++ src/piper_control_ros2/piper_control_ros2/piper_control_node.py
@@ -24,7 +24,7 @@ JOINT_NAMES = ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6"]
 
 # Control parameters
 CONTROL_HZ = 200
-DEFAULT_KP_GAINS = (5.5, 5.5, 15.0, 15.0, 25.0, 15.0)
+DEFAULT_KP_GAINS = (5.5, 5.5, 10.0, 15.0, 25.0, 15.0)
 DEFAULT_KD_GAINS = (0.8, 0.8, 0.8, 0.8, 0.8, 0.8)
 
 
