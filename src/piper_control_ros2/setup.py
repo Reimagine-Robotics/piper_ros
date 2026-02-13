83840f58224b50d0cc7b6e77cebe3eacfc6d312f
Akhil Raju <akhilraju@gmail.com>
2026-02-13 11:30:17 +0000

    Upgrade piper-control again.

--- src/piper_control_ros2/setup.py
+++ src/piper_control_ros2/setup.py
@@ -20,7 +20,7 @@ package_name = "piper_control_ros2"
 
 setup(
     name=package_name,
-    version="1.2.1",
+    version="1.2.2",
     packages=find_packages(exclude=["test"]),
     data_files=[
         (
