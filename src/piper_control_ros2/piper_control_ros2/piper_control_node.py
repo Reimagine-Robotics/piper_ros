103e2967c593747af89fb1844ac50b87f42014a7
JafarAbdi <jafar.uruc@gmail.com>
2026-03-18 15:38:12 +0000

    Support target velocities

--- src/piper_control_ros2/piper_control_ros2/piper_control_node.py
+++ src/piper_control_ros2/piper_control_ros2/piper_control_node.py
@@ -359,9 +359,9 @@ class PiperControlNode(Node):
     efforts = list(joint_command.efforts)
 
     if positions:
-      if velocities or efforts:
+      if efforts:
         self.get_logger().warn(
-            "Received joint positions, but also velocities or efforts",
+            "Received joint positions, but also efforts",
         )
 
       self.get_logger().debug(f"Received joint positions: {positions}")
@@ -394,6 +394,7 @@ class PiperControlNode(Node):
           kp_gains=kp_gains,
           kd_gains=kd_gains,
           torques_ff=torque,
+          velocities=velocities,
       )
 
     elif velocities:
